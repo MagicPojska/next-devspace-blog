869ba116df865b78ca9deecf5c7b87c8bd835082
MagicPojska <pojskicsafet@gmail.com>
2021-10-10 13:38:05 +0200

    Added category sidebar

--- pages/blog/page/[page_index].jsx
+++ pages/blog/page/[page_index].jsx
@@ -2,20 +2,31 @@ import Layout from "../../../components/Layout"
 import fs from 'fs'
 import path from "path"
 import Post from "../../../components/Post"
+import CategoryList from '../../../components/CategoryList'
 import Pagination from "../../../components/Pagination"
 import { POSTS_PER_PAGE } from "../../../config/index"
 import { getPosts } from "../../../lib/posts"
 
-export default function BlogPage({ posts, numPages, currentPage }) {
+export default function BlogPage({ posts, numPages, currentPage, categories }) {
     return (
         <Layout>
-            <h1 className='text-5xl border-b-4 p-5 font-bold'>All Posts</h1>
+            <div className='flex justify-between flex-col md:flex-row'>
+                <div className='w-3/4 mr-10'>
+                    <h1 className='text-5xl border-b-4 p-5 font-bold'>Blog</h1>
 
-            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
-                {posts.map((post, index) => <Post key={index} post={post} />)}
+                    <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                        {posts.map((post, index) => (
+                            <Post key={index} post={post} />
+                        ))}
+                    </div>
+
+                    <Pagination currentPage={currentPage} numPages={numPages} />
+                </div>
+
+                <div className='w-1/4'>
+                    <CategoryList categories={categories} />
+                </div>
             </div>
-
-            <Pagination currentPage={currentPage} numPages={numPages} />
         </Layout>
     )
 }
@@ -48,6 +59,11 @@ export async function getStaticProps({ params }) {
 
     const posts = getPosts()
 
+    // Get categories for sidebar
+    const categories = posts.map(post => post.frontmatter.category)
+
+    const uniqueCategories = [...new Set(categories)]
+
     const numPages = Math.ceil(files.length / POSTS_PER_PAGE)
     const pageIndex = page - 1
     const orderedPosts = posts
@@ -57,7 +73,8 @@ export async function getStaticProps({ params }) {
         props: {
             posts: orderedPosts,
             numPages,
-            currentPage: page
+            currentPage: page,
+            categories: uniqueCategories
         }
     }
 }
